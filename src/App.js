fcfb41c0bcd3a9661ec58ce9c74e3ceafab00ff0
vivekkasturi <kasturi.vivek994@gmail.com>
2023-07-27 00:22:21 +0530

    password html layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+//container
+//header
+//copy button
+//charaterlength and number
+//length-bar
+//4 checkboxs with text's
+//generate pasword button
 function App() {
+
+  const checkboxdata = [{ Name: "UpperCase", state: false },
+  { Name: "LowerCase", state: false },
+  { Name: "UpperandNumberCase", state: false },
+  { Name: "LowerandNumberCase", state: false }];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Conatiner">
+      <div className="Header">
+        <h2>headertext</h2>
+        <button>Copy</button>
+      </div>
+
+      <div className="CharLength">
+        <h3>Character Length</h3>
+        <span>number</span>
+      </div>
+      <div className="CharLengthBar">
+
+        <input type="range" />
+
+      </div>
+      <div className="Checkboxwithtext">
+
+        {checkboxdata.map((element, index) => {
+
+          return (
+
+            <div>
+              <li key={index}> <input type="checkbox" value={element.state} />{element.Name}</li>
+            </div>
+          )
+        })}
+      </div>
+      <div className="GeneratePasswordBtn">
+        <button>Generate Password</button>
+      </div>
     </div>
   );
 }
